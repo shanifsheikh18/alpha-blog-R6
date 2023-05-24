2e039e1aaa3c3ad1ff99bd98d50f4aba4f41f7b3
shanif <shanifsheikh.18@gmail.com>
2023-05-24 15:47:06 +0500

    alpha-blog till section 5

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# It will remove the error_div added by the form(rails)
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
