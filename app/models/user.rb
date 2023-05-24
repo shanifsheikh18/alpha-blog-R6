428cc98b7d36790919f39c2e4c52c6e85177a298
shanif <shanifsheikh.18@gmail.com>
2023-05-24 16:51:42 +0500

    done with user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-    
+   # const -->> VALID_EMAIL_REGEX 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :username, presence: true,
+                         uniqueness: { case_sensitive: false },
+                         length: { minimum: 3, maximum: 25 }
+    validates :email, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
 end
